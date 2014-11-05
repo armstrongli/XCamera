9f9b8803a5ec7aedada1ad10868e9e375ade8242
armstrongli <jqxxboy@gmail.com>
2014-11-05 20:28:05 +0800

    change XCamera to call primary camera app.

--- src/com/xxboy/xcamera/XCamera.java
+++ src/com/xxboy/xcamera/XCamera.java
@@ -1,9 +1,6 @@
 package com.xxboy.xcamera;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -18,100 +15,106 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.xxboy.photo.R;
 import com.xxboy.common.CommonFunction;
+import com.xxboy.photo.R;
 
 public class XCamera extends Activity {
-    private Button button;
+	private Button button;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo);
-        button = (Button) findViewById(R.id.btn_camera);
-        button.setOnClickListener(new OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_photo);
+		button = (Button) findViewById(R.id.btn_camera);
+		button.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setAction(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+				// intent.putExtra(MediaStore.EXTRA_OUTPUT,
+				// Uri.fromFile(new File(getFullFileName())));
 
-                startActivityForResult(intent, 1);
-            }
-        });
-    }
+				// return to XCamera after take photos
+				startActivityForResult(intent, 1);
+			}
+		});
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            String sdStatus = Environment.getExternalStorageState();
-            if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
-                Log.i("TestFile", "SD card is not avaiable/writeable right now.");
-                return;
-            }
+	private String generateFullFileName() {
+		/** get picture folder and create system locale date folder */
+		File pictureFolder = new File(getString(R.string.picture_folder_path));
+		if (!pictureFolder.exists()) {
+			pictureFolder.mkdirs();
+		}
 
-            /** get picture folder and create system locale date folder */
-            File pictureFolder = new File(getString(R.string.picture_folder_path));
-            if (!pictureFolder.exists()) {
-                pictureFolder.mkdirs();
-            }
+		String fileNameDatePart = CommonFunction.getCurrentDateString();
+		String folderName = fileNameDatePart.substring(0, 8);
+		String targetFolderFullPath = pictureFolder.getPath() + File.separator
+				+ folderName;
+		File targetFolder = new File(targetFolderFullPath);
+		if (!targetFolder.exists()) {
+			targetFolder.mkdirs();
+		}
 
-            String fileNameDatePart = CommonFunction.getCurrentDateString();
-            String folderName = fileNameDatePart.substring(0, 8);
-            String targetFolderFullPath = pictureFolder.getPath() + File.separator + folderName;
-            File targetFolder = new File(targetFolderFullPath);
-            if (!targetFolder.exists()) {
-                targetFolder.mkdirs();
-            }
+		/** prepare file name */
+		String photoFullName = getString(R.string.picture_prefix)
+				+ fileNameDatePart + getString(R.string.picture_cofix);
+		return targetFolder.getPath() + File.separator + photoFullName;
+	}
 
-            /** prepare file name */
-            String photoFullName = getString(R.string.picture_prefix) + fileNameDatePart
-                    + getString(R.string.picture_cofix);
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == Activity.RESULT_OK) {
+			String sdStatus = Environment.getExternalStorageState();
+			if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+				Log.i("TestFile",
+						"SD card is not avaiable/writeable right now.");
+				return;
+			}
 
-            Toast.makeText(this, photoFullName, Toast.LENGTH_LONG).show();
-            Bundle bundle = data.getExtras();
-            Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
+			// String photoFullName = getFullFileName();
 
-            FileOutputStream b = null;
-            String fileName = targetFolder.getPath() + File.separator + photoFullName;
+			Bundle bundle = data.getExtras();
+			Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
 
-            try {
-                b = new FileOutputStream(fileName);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    b.flush();
-                    b.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            ((ImageView) findViewById(R.id.img_photo)).setImageBitmap(bitmap);// 将图片显示在ImageView里
-        }
-    }
+			// FileOutputStream b = null;
+			//
+			// try {
+			// b = new FileOutputStream(photoFullName);
+			// bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+			// } catch (FileNotFoundException e) {
+			// e.printStackTrace();
+			// } finally {
+			// try {
+			// b.flush();
+			// b.close();
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// }
+			// }
+			((ImageView) findViewById(R.id.img_photo)).setImageBitmap(bitmap);// 将图片显示在ImageView里
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.photo, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.photo, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
