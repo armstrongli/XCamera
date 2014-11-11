4d865d6146b8a47d3ffe69f3fb90b39e3d50d328
armstrongli <jqxxboy@gmail.com>
2014-11-11 22:10:51 +0800

    remove elements of comments

--- src/com/xxboy/common/XFunction.java
+++ src/com/xxboy/common/XFunction.java
@@ -6,53 +6,34 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import com.xxboy.xcamera.XCamera;
+
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 
 public final class XFunction {
+	/**
+	 * calculate and zoom pic.
+	 * 
+	 * @author Armstrong
+	 * 
+	 */
 	public static final class XCompress {
 		public static final CompressFormat JPEG = Bitmap.CompressFormat.JPEG;
 
-		public final static Bitmap compressImage(Bitmap image) {
+		private final static Bitmap compressImage(Bitmap image) {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			image.compress(JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+			image.compress(JPEG, 100, baos);
 			int options = 100;
-			while (baos.toByteArray().length / 1024 > 100) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
-				baos.reset();// 重置baos即清空baos
-				image.compress(JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
-				options -= 10;// 每次都减少10
+			while (baos.toByteArray().length / 1024 > 100) {
+				baos.reset();
+				image.compress(JPEG, options, baos);
+				options -= 10;
 			}
-			ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
-			Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+			ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
+			Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);
 			return bitmap;
-		}
-
-		public final static Bitmap getimage(String srcPath) {
-			BitmapFactory.Options newOpts = new BitmapFactory.Options();
-			// 开始读入图片，此时把options.inJustDecodeBounds 设回true了
-			newOpts.inJustDecodeBounds = true;
-			Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);// 此时返回bm为空
-
-			newOpts.inJustDecodeBounds = false;
-			int w = newOpts.outWidth;
-			int h = newOpts.outHeight;
-			// 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
-			float hh = 800f;// 这里设置高度为800f
-			float ww = 480f;// 这里设置宽度为480f
-			// 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
-			int be = 1;// be=1表示不缩放
-			if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
-				be = (int) (newOpts.outWidth / ww);
-			} else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
-				be = (int) (newOpts.outHeight / hh);
-			}
-			if (be <= 0)
-				be = 1;
-			newOpts.inSampleSize = be;// 设置缩放比例
-			// 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
-			bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
-			return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
 		}
 
 		/**
@@ -71,29 +52,26 @@ public final class XFunction {
 			}
 			ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
 			BitmapFactory.Options newOpts = new BitmapFactory.Options();
-			// 开始读入图片，此时把options.inJustDecodeBounds 设回true了
 			newOpts.inJustDecodeBounds = true;
 			Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
-			newOpts.inJustDecodeBounds = false;
-			int w = newOpts.outWidth;
-			int h = newOpts.outHeight;
-			// 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
-			float hh = 800f;// 这里设置高度为800f
-			float ww = 480f;// 这里设置宽度为480f
-			// 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
-			int be = 1;// be=1表示不缩放
-			if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
-				be = (int) (newOpts.outWidth / ww);
-			} else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
-				be = (int) (newOpts.outHeight / hh);
+			int picWidth = newOpts.outWidth;
+			int picHeight = newOpts.outHeight;
+			// set the with and height as the screen size.
+			float screenHeight = XCamera.XCameraConst.SCREEN_HEIGHT;
+			float screenWidth = XCamera.XCameraConst.SCREEN_WIDTH;
+			int zoomPercentage = 1;
+			if (picWidth > picHeight && picWidth > screenWidth) {
+				zoomPercentage = (int) (picWidth / screenWidth);
+			} else if (picWidth < picHeight && picHeight > screenHeight) {
+				zoomPercentage = (int) (picHeight / screenHeight);
 			}
-			if (be <= 0)
-				be = 1;
-			newOpts.inSampleSize = be;// 设置缩放比例
-			// 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+			if (zoomPercentage <= 0)
+				zoomPercentage = 1;
+			newOpts.inSampleSize = zoomPercentage;
+			newOpts.inJustDecodeBounds = false;
 			isBm = new ByteArrayInputStream(baos.toByteArray());
 			bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
-			return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
+			return compressImage(bitmap);
 		}
 	}
 
--- src/com/xxboy/xcamera/XCamera.java
+++ src/com/xxboy/xcamera/XCamera.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 import android.app.Activity;
 import android.content.Intent;
