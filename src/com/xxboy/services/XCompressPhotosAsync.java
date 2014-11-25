6da77eea2ae0950c965204904f436ad869ad1690
armstrongli <jqxxboy@gmail.com>
2014-11-25 20:40:07 +0800

    finish compress image async tasks

--- src/com/xxboy/services/XCompressPhotosAsync.java
+++ src/com/xxboy/services/XCompressPhotosAsync.java
@@ -1,21 +1,70 @@
 package com.xxboy.services;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 
 import com.xxboy.common.XFunction;
+import com.xxboy.log.Logger;
 
-public class XCompressPhotosAsync extends AsyncTask<File, Void, Integer> {
+/**
+ * xcamera folder
+ * 
+ * @author Armstrong
+ * 
+ */
+public class XCompressPhotosAsync extends AsyncTask<Void, Void, Integer> {
+
+	public static class XPhotoParam {
+		private String xPath;
+		private String xCachePath;
+
+		public String getxPath() {
+			return xPath;
+		}
+
+		public void setxPath(String xPath) {
+			this.xPath = xPath;
+		}
+
+		public String getxCachePath() {
+			return xCachePath;
+		}
+
+		public void setxCachePath(String xCachePath) {
+			this.xCachePath = xCachePath;
+		}
+
+	}
+
+	private XPhotoParam param;
+
+	public XCompressPhotosAsync(XPhotoParam param) {
+		super();
+		this.param = param;
+	}
 
 	@Override
-	protected Integer doInBackground(File... params) {
-		if (params == null || params.length == 0) {
-			return 0;
-		}
+	protected Integer doInBackground(Void... params) {
+		String xCameraPath = this.param.getxPath();
+		String xCachePath = this.param.getxCachePath();
+
 		Integer count = 0;
-		for (File xFolder : params) {
+		File cameraFolder = new File(xCameraPath);
+		if (!cameraFolder.exists()) {
+			return count;
+		}
+
+		File[] xFolders = cameraFolder.listFiles();
+		for (File xFolder : xFolders) {
+			if (!xFolder.isDirectory() || xFolder.isHidden()) {
+				continue;
+			}
 			File[] pictures = xFolder.listFiles();
 			if (pictures == null || pictures.length == 0) {
 				continue;
@@ -27,11 +76,22 @@ public class XCompressPhotosAsync extends AsyncTask<File, Void, Integer> {
 					continue;
 				} else {
 					count++;
-					Bitmap compressed = XFunction.XCompress.comp(image);
+					Bitmap compressed = XFunction.XCompress.comp(BitmapFactory.decodeFile(picture.getAbsolutePath()));
+
+					try {
+						File file = new File(picture.getAbsolutePath().replaceAll(xCameraPath, xCachePath));
+						FileOutputStream out = new FileOutputStream(file);
+						compressed.compress(Bitmap.CompressFormat.JPEG, 100, out);
+						out.flush();
+						out.close();
+					} catch (FileNotFoundException e) {
+						Logger.log(e);
+					} catch (IOException e) {
+						Logger.log(e);
+					}
 				}
 			}
 		}
 		return null;
 	}
-
 }
