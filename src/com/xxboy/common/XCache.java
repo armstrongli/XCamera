5bf7c81bde4ce474a2eb271901e6d937d694b2f3
Armstrong <armstrongli@vmware.com>
2015-01-01 14:34:03 +0800

    add disk cache

--- src/com/xxboy/common/XCache.java
+++ src/com/xxboy/common/XCache.java
@@ -1,9 +1,18 @@
 package com.xxboy.common;
 
-import com.xxboy.log.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.LruCache;
+
+import com.xxboy.log.Logger;
+import com.xxboy.xcamera.XCamera.XCameraConst;
 
 public class XCache {
 	private static final int M_MEMORY_CACHE_SIZE = 10 * 1024 * 1024;// 10M
@@ -27,4 +36,83 @@ public class XCache {
 	}
 
 	private static final int M_DISK_CACHE_SIZE = 20 * 1024 * 1024;// 20M
+	private static DiskLruCache mDiskCache;
+
+	public static Bitmap getFromDiskCache(String id) {
+		try {
+			DiskLruCache.Editor editor = getDiskCache().edit(hashKeyForDisk(id));
+			if (editor != null) {
+				InputStream input = editor.newInputStream(0);
+				Bitmap bitmap = BitmapFactory.decodeStream(input);
+				if (input != null) {
+					input.close();
+				}
+				return bitmap;
+			}
+		} catch (IOException e) {
+			Logger.log(e.getMessage(), e);
+		} catch (Exception e) {
+			Logger.log(e.getMessage(), e);
+		}
+		return null;
+	}
+
+	public static void push2DiskCache(String id, Bitmap bitmap) {
+		Bitmap cachedBitmap = getFromDiskCache(id);
+		if (cachedBitmap != null) {
+			return;
+		}
+		try {
+			DiskLruCache.Editor editor = getDiskCache().edit(hashKeyForDisk(id));
+			if (editor != null) {
+				OutputStream outputStream = editor.newOutputStream(0);
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream);
+				editor.commit();
+				if (outputStream != null) {
+					outputStream.close();
+				}
+
+				getDiskCache().flush();
+			}
+		} catch (IOException e) {
+			Logger.log(e.getMessage(), e);
+		} catch (Exception e) {
+			Logger.log(e.getMessage(), e);
+		}
+	}
+
+	private static final DiskLruCache getDiskCache() {
+		if (mDiskCache == null || mDiskCache.isClosed()) {
+			try {
+				mDiskCache = DiskLruCache.open(new File(XCameraConst.xCachePath), 1, 1, M_DISK_CACHE_SIZE);
+			} catch (IOException e) {
+				Logger.log(e.getMessage(), e);
+			}
+		}
+		return mDiskCache;
+	}
+
+	private static final String hashKeyForDisk(String key) {
+		String cacheKey;
+		try {
+			final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+			mDigest.update(key.getBytes());
+			cacheKey = bytesToHexString(mDigest.digest());
+		} catch (NoSuchAlgorithmException e) {
+			cacheKey = String.valueOf(key.hashCode());
+		}
+		return cacheKey;
+	}
+
+	private static final String bytesToHexString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < bytes.length; i++) {
+			String hex = Integer.toHexString(0xFF & bytes[i]);
+			if (hex.length() == 1) {
+				sb.append('0');
+			}
+			sb.append(hex);
+		}
+		return sb.toString();
+	}
 }
