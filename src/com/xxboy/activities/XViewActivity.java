17b868c575c23cb6badf17a6d042ac55e94aece3
armstrongli <jqxxboy@gmail.com>
2015-01-29 22:08:59 +0800

    reset the pic show algorithm.

--- src/com/xxboy/activities/XViewActivity.java
+++ src/com/xxboy/activities/XViewActivity.java
@@ -28,28 +28,28 @@ public class XViewActivity extends Activity {
 		opt.inJustDecodeBounds = true;
 		BitmapFactory.decodeFile(resourcePath, opt);
 		float width_divide_height = opt.outHeight / opt.outWidth;
-		int cal_width = 0, cal_height = 0;
+		double cal_width = 0, cal_height = 0;
 		if (width_divide_height > XCameraConst.WIDTH_DIVIDE_HEIGHT) {
-			if (opt.outWidth > XCameraConst.PHOTO_ITEM_WIDTH) {
-				cal_width = XCameraConst.PHOTO_ITEM_WIDTH;
+			if (opt.outWidth > XCameraConst.SCREEN_WIDTH) {
+				cal_width = XCameraConst.SCREEN_WIDTH;
 				cal_height = cal_width * (opt.outHeight / opt.outWidth);
 			} else {
 				cal_height = opt.outHeight;
 				cal_width = opt.outWidth;
 			}
-			opt.inSampleSize = opt.outHeight / cal_height;
+			opt.inSampleSize = (int) (opt.outHeight / cal_height);
 		} else {
-			if (opt.outHeight > XCameraConst.PHOTO_ITEM_HEIGHT) {
-				cal_height = XCameraConst.PHOTO_ITEM_HEIGHT;
+			if (opt.outHeight > XCameraConst.SCREEN_HEIGHT) {
+				cal_height = XCameraConst.SCREEN_HEIGHT;
 				cal_width = cal_height * (opt.outWidth / opt.outHeight);
 			} else {
 				cal_height = opt.outHeight;
 				cal_width = opt.outWidth;
 			}
-			opt.inSampleSize = opt.outWidth / cal_width;
+			opt.inSampleSize = (int) (opt.outWidth / cal_width);
 		}
-		opt.outHeight = cal_height;
-		opt.outWidth = cal_width;
+		opt.outHeight = (int) (cal_height);
+		opt.outWidth = (int) (cal_width);
 
 		opt.inSampleSize = opt.inSampleSize;
 
