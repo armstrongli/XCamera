22790fde04ac39431c49202727bcd92e07e27ac5
armstrongli <jqxxboy@gmail.com>
2015-02-25 22:09:26 +0800

    add touch listener, though it doesn't work

---
+++ src/com/xxboy/listeners/XViewTouchListener.java
@@ -0,0 +1,15 @@
+package com.xxboy.listeners;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class XViewTouchListener implements OnTouchListener {
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
