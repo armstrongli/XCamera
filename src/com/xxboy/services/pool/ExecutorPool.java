5eca7cdafc16eb95c12312bc21177d550b460468
armstrongli <jqxxboy@gmail.com>
2015-03-25 22:25:20 +0800

    add checker for moving unneeded task to waiting pool.

--- src/com/xxboy/services/pool/ExecutorPool.java
+++ src/com/xxboy/services/pool/ExecutorPool.java
@@ -1,5 +1,6 @@
 package com.xxboy.services.pool;
 
+import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.xxboy.services.runnable.ImageExecutor;
@@ -8,7 +9,7 @@ public class ExecutorPool {
 
 	private static Object lock = new Object();
 
-	private static ConcurrentHashMap<String, Integer> path2Position = new ConcurrentHashMap<String, Integer>();
+	private static ConcurrentHashMap<String, Integer> runningPath2Position = new ConcurrentHashMap<String, Integer>();
 
 	private static ConcurrentHashMap<String, ImageExecutor> waitingImageExecutorPool = new ConcurrentHashMap<String, ImageExecutor>();
 
@@ -17,33 +18,52 @@ public class ExecutorPool {
 	public static final void executeExecutor(ImageExecutor executor) {
 		String path = executor.getImagePath();
 		int position = executor.getPosition();
-		if (!path2Position.containsKey(path)) {
-			path2Position.put(path, position);
-		}
 		synchronized (lock) {
+			if (!runningPath2Position.containsKey(path)) {
+				runningPath2Position.put(path, position);
+			}
 			if (runningImageExecutorPool.contains(path)) {
 				ImageExecutor tmpExecutor = runningImageExecutorPool.get(path);
 				tmpExecutor.stop();
 				tmpExecutor.setImageView(executor.getImageView());
-				tmpExecutor.setPosition(executor.getPosition());
+				tmpExecutor.setPosition(position);
 			} else if (waitingImageExecutorPool.containsKey(path)) {
 				ImageExecutor tmpExecutor = waitingImageExecutorPool.get(path);
 				tmpExecutor.setImageView(executor.getImageView());
-				tmpExecutor.setPosition(executor.getPosition());
+				tmpExecutor.setPosition(position);
 				moveToRunningExecutorPool(path);
 			} else {
 				runningImageExecutorPool.put(path, executor);
 			}
-			if (RunnablePool.checkCanBeRan(executor.getPosition())) {
+			if (RunnablePool.checkCanBeRan(position)) {
 				runningImageExecutorPool.get(path).start();
 			} else {
 				moveToWaitingExecutor(path);
+			}
+
+			// -- check whether the position has been over dual. if it is, move to waiting pool
+			Collection<Integer> runningPathes = runningPath2Position.values();
+			boolean needMoveToWaitingPool = false;
+			for (Integer item : runningPathes) {
+				boolean canBeRan = RunnablePool.checkCanBeRan(item);
+				if (!canBeRan) {
+					needMoveToWaitingPool = true;
+					break;
+				}
+			}
+			if (needMoveToWaitingPool) {
+				for (String item : runningImageExecutorPool.keySet()) {
+					if (!RunnablePool.checkCanBeRan(runningPath2Position.get(item))) {
+						moveToWaitingExecutor(path);
+					}
+				}
 			}
 		}
 	}
 
 	private static final void moveToWaitingExecutor(String imagePath) {
 		synchronized (lock) {
+			runningPath2Position.remove(imagePath);
 			ImageExecutor executor = runningImageExecutorPool.remove(imagePath);
 			if (executor != null) {
 				waitingImageExecutorPool.put(imagePath, executor);
@@ -62,6 +82,7 @@ public class ExecutorPool {
 
 	public static final void removeExecutor(String imagePath) {
 		synchronized (lock) {
+			runningPath2Position.remove(imagePath);
 			runningImageExecutorPool.remove(imagePath);
 		}
 	}
@@ -77,7 +98,7 @@ public class ExecutorPool {
 			for (ImageExecutor item : waitingImageExecutorPool.values()) {
 				item.stop();
 			}
-			path2Position.clear();
+			runningPath2Position.clear();
 			runningImageExecutorPool.clear();
 			waitingImageExecutorPool.clear();
 		}
