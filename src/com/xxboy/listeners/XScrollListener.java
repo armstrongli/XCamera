7af809ac72af7eb827ecbf6dfa8cdb0ca28d6abb
Armstrong Li <jqxxboy@gmail.com>
2015-03-26 14:43:50 +0800

    add sync indexes function for grid view scroll.

--- src/com/xxboy/listeners/XScrollListener.java
+++ src/com/xxboy/listeners/XScrollListener.java
@@ -4,6 +4,7 @@ import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 
 import com.xxboy.log.Logger;
+import com.xxboy.services.pool.RunnablePool;
 import com.xxboy.utils.XQueueUtil;
 import com.xxboy.xcamera.XCamera.XCameraConst;
 
@@ -18,6 +19,7 @@ public class XScrollListener implements OnScrollListener {
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 		// change loading picture and recycle picture resource.
+		RunnablePool.syncRunnableIndexes(firstVisibleItem, firstVisibleItem + visibleItemCount);
 		XQueueUtil.syncVisableIndexes(firstVisibleItem, firstVisibleItem + visibleItemCount);
 		if (XCameraConst.GLOBAL_X_GRIDVIEW_VISIABLE_COUNT < visibleItemCount) {
 			XCameraConst.GLOBAL_X_GRIDVIEW_VISIABLE_COUNT = visibleItemCount;
