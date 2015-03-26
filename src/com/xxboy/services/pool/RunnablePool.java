a218c881eae56f40427d9f53037d71ade8e4e251
Armstrong Li <jqxxboy@gmail.com>
2015-03-26 14:45:14 +0800

    add comments for runnable pool.

--- src/com/xxboy/services/pool/RunnablePool.java
+++ src/com/xxboy/services/pool/RunnablePool.java
@@ -5,6 +5,12 @@ public final class RunnablePool {
 	private static int endIndex = 0;
 	private static Object indexLock = new Object();
 
+	/**
+	 * sync view item start position and end position.
+	 * 
+	 * @param newStartIndex
+	 * @param newEndIndex
+	 */
 	public static void syncRunnableIndexes(int newStartIndex, int newEndIndex) {
 		synchronized (indexLock) {
 			startIndex = newStartIndex;
@@ -12,6 +18,12 @@ public final class RunnablePool {
 		}
 	}
 
+	/**
+	 * check whether the exact position is shown
+	 * 
+	 * @param toBeRanIndex
+	 * @return
+	 */
 	public static boolean checkCanBeRan(int toBeRanIndex) {
 		synchronized (indexLock) {
 			return toBeRanIndex >= startIndex && toBeRanIndex <= endIndex;
