dfc53e89e8460306567d392446c604958bd02c1e
armstrongli <jqxxboy@gmail.com>
2015-04-01 22:28:53 +0800

    change the calculation method

--- src/com/xxboy/utils/XBitmapUtil.java
+++ src/com/xxboy/utils/XBitmapUtil.java
@@ -49,8 +49,8 @@ public class XBitmapUtil {
 
 			// Calculate the largest inSampleSize value that is a power of 2 and keeps both
 			// height and width larger than the requested height and width.
-			while ((halfHeight / inSampleSize) > reqHeight || (halfWidth / inSampleSize) > reqWidth) {
-				inSampleSize *= 2;
+			while (halfHeight > inSampleSize * reqHeight || halfWidth > inSampleSize * reqWidth) {
+				inSampleSize <<= 1;
 			}
 		}
 
