2212045854b82e6a60a91ecafffbd5f213987133
armstrongli <jqxxboy@gmail.com>
2015-04-07 21:03:05 +0800

    change mem cache key to integer

--- src/com/xxboy/utils/XCacheUtil.java
+++ src/com/xxboy/utils/XCacheUtil.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,9 +15,9 @@ import com.xxboy.log.Logger;
 
 public class XCacheUtil {
 	private static final Long M_MEMORY_CACHE_SIZE = Runtime.getRuntime().maxMemory() / 6;// 1/6 of runtime max memory
-	private static LruCache<String, Bitmap> mMemoryCache = new LruCache<String, Bitmap>(M_MEMORY_CACHE_SIZE.intValue()) {
+	private static LruCache<Integer, Bitmap> mMemoryCache = new LruCache<Integer, Bitmap>(M_MEMORY_CACHE_SIZE.intValue()) {
 		@Override
-		protected int sizeOf(String key, Bitmap value) {
+		protected int sizeOf(Integer key, Bitmap value) {
 			if (value == null || value.isRecycled()) {
 				mMemoryCache.remove(key);
 				return 0;
@@ -28,20 +26,29 @@ public class XCacheUtil {
 			}
 		}
 
+		@Override
+		protected void entryRemoved(boolean evicted, Integer key, Bitmap oldValue, Bitmap newValue) {
+			Logger.log("removing from mem cache: " + key);
+			super.entryRemoved(evicted, key, oldValue, newValue);
+			pushToDiskCache(key, oldValue);
+		}
+
 	};
 
-	public static final Bitmap getFromCache(String id) {
-		Bitmap bitmapFromMem = getFromMemCache(id);
+	public static final Bitmap getFromCache(final String path) {
+		final int hashKey = HashKeyUtil.hashKey(path);
+		final Bitmap bitmapFromMem = getFromMemCache(path);
 		if (bitmapFromMem != null && !bitmapFromMem.isRecycled()) {
-			Logger.debug("hit in cache: memory cache");
+			Logger.log("hit in cache: memory cache");
 			return bitmapFromMem;
 		} else {
-			Bitmap bitmapFromDisk = getFromDiskCache(id);
+			final Bitmap bitmapFromDisk = getFromDiskCache(path);
 			if (bitmapFromDisk != null && !bitmapFromDisk.isRecycled()) {
-				pushToMemCache(id, bitmapFromDisk);
-				Logger.debug("hit in cache: disk cache");
+				pushToMemCache(hashKey, bitmapFromDisk);
+				Logger.log("hit in cache: disk cache");
 				return bitmapFromDisk;
 			} else {
+				Logger.log("hit empty " + path);
 				return null;
 			}
 		}
@@ -50,25 +57,26 @@ public class XCacheUtil {
 	/**
 	 * push to mem cache & disk cache
 	 * 
-	 * @param id
+	 * @param path
 	 * @param bitmap
 	 */
-	public static final Bitmap pushToCache(String id, Bitmap bitmap) {
-		Bitmap bitmapFromMem = getFromMemCache(id);
+	public static final Bitmap pushToCache(final String path, final Bitmap bitmap) {
+		final int hashKey = HashKeyUtil.hashKey(path);
+		Bitmap bitmapFromMem = getFromMemCache(path);
 		if (bitmapFromMem == null) {
-			pushToMemCache(id, bitmap);
+			pushToMemCache(hashKey, bitmap);
 		}
-		Bitmap bitmapFromDisk = getFromDiskCache(id);
+		Bitmap bitmapFromDisk = getFromDiskCache(path);
 		if (bitmapFromDisk == null) {
-			pushToDiskCache(id, bitmap);
+			pushToDiskCache(hashKey, bitmap);
 		}
 		return bitmap;
 	}
 
-	public static void pushToMemCache(String id, Bitmap bitmap) {
-		Logger.debug("Pushing to memory cache: " + id);
+	private static void pushToMemCache(final Integer hashKey, final Bitmap bitmap) {
+		Logger.debug("Pushing to memory cache: " + hashKey);
 		try {
-			mMemoryCache.put(hashKeyForDisk(id), bitmap);
+			mMemoryCache.put(hashKey, bitmap);
 		} catch (Exception e) {
 			Logger.log(e.getMessage(), e);
 		}
@@ -82,30 +90,31 @@ public class XCacheUtil {
 		return getFromDiskCache(fullImageViewId);
 	}
 
-	public static final void pushImageView(String id, Bitmap bitmap) {
-		String fullImageViewId = FULL_IMAGEVIEW_PREFIX + id;
-		pushToDiskCache(fullImageViewId, bitmap);
+	public static final void pushImageView(final String id, final Bitmap bitmap) {
+		final String fullImageViewId = FULL_IMAGEVIEW_PREFIX + id;
+		final int hashKey = HashKeyUtil.hashKey(fullImageViewId);
+		pushToDiskCache(hashKey, bitmap);
 	}
 
 	/**
 	 * getting bitmap from hard memory cache.
 	 * 
-	 * @param id
+	 * @param path
 	 *            the primary path id of image.
 	 * @return
 	 */
-	public static Bitmap getFromMemCache(String id) {
+	public static Bitmap getFromMemCache(String path) {
 		try {
-			Logger.debug("Getting From memcache(" + mMemoryCache.size() + "): " + id);
+			Logger.debug("Getting From memcache(" + mMemoryCache.size() + "): " + path);
 			// check whether it's in memory cache
-			Bitmap bitmap = mMemoryCache.get(hashKeyForDisk(id));
+			Bitmap bitmap = mMemoryCache.get(HashKeyUtil.hashKey(path));
 			if (bitmap != null && bitmap.isRecycled()) {
 				Logger.debug("hit in cache: memory cache");
 				return bitmap;
 			}
 			return null;
 		} catch (Exception e) {
-			Logger.log("Error when getting bitmap from memory cache with id: " + id, e);
+			Logger.log("Error when getting bitmap from memory cache with id: " + path, e);
 		}
 		return null;
 	}
@@ -113,10 +122,10 @@ public class XCacheUtil {
 	private static final Long M_DISK_CACHE_SIZE = 20l * 1024l * 1024l;// 20M
 	private static DiskLruCache mDiskCache;
 
-	private static Bitmap getFromDiskCache(String id) {
-		Logger.debug("Getting from diskCache: " + id);
+	private static Bitmap getFromDiskCache(String path) {
+		Logger.debug("Getting from diskCache: " + path);
 		try {
-			DiskLruCache.Snapshot snapshot = getDiskCache().get(hashKeyForDisk(id));
+			DiskLruCache.Snapshot snapshot = getDiskCache().get(HashKeyUtil.hashKey(path).toString());
 			if (snapshot != null) {
 				InputStream bitmapInputStream = snapshot.getInputStream(0);
 				try {
@@ -137,15 +146,14 @@ public class XCacheUtil {
 		return null;
 	}
 
-	private static void pushToDiskCache(String id, Bitmap bitmap) {
+	private static void pushToDiskCache(final Integer hashKey, final Bitmap bitmap) {
 		if (bitmap == null || bitmap.isRecycled()) {
 			return;
 		}
-		Logger.debug("Pushing to disk cache: " + id);
+		Logger.debug("Pushing to disk cache: " + hashKey);
 		try {
-			String editorKey = hashKeyForDisk(id);
-			Logger.debug("Editor key is: " + editorKey);
-			DiskLruCache.Editor editor = getDiskCache().edit(editorKey);
+			Logger.debug("Editor key is: " + hashKey);
+			DiskLruCache.Editor editor = getDiskCache().edit(hashKey.toString());
 			if (editor != null) {
 				OutputStream outputStream = editor.newOutputStream(0);
 				bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream);
@@ -174,33 +182,39 @@ public class XCacheUtil {
 		return mDiskCache;
 	}
 
-	private static final String hashKeyForDisk(String key) {
-		String cacheKey;
-		MessageDigest mDigest = getMsgDgst();
-		mDigest.update(key.getBytes());
-		cacheKey = bytesToHexString(mDigest.digest());
-		return cacheKey;
-	}
-
-	private static final MessageDigest getMsgDgst() {
-		try {
-			return MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			Logger.log(e);
+	private static final class HashKeyUtil {
+		public static final Integer hashKey(String path) {
+			return path.hashCode();
 		}
-		return null;
-	}
 
-	private static final String bytesToHexString(byte[] bytes) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < bytes.length; i++) {
-			String hex = Integer.toHexString(0xFF & bytes[i]);
-			if (hex.length() == 1) {
-				sb.append('0');
-			}
-			sb.append(hex);
-		}
-		return sb.toString();
+		// private static final String hashKeyForDisk(String key) {
+		// String cacheKey;
+		// MessageDigest mDigest = getMsgDgst();
+		// mDigest.update(key.getBytes());
+		// cacheKey = bytesToHexString(mDigest.digest());
+		// return cacheKey;
+		// }
+		//
+		// private static final MessageDigest getMsgDgst() {
+		// try {
+		// return MessageDigest.getInstance("MD5");
+		// } catch (NoSuchAlgorithmException e) {
+		// Logger.log(e);
+		// }
+		// return null;
+		// }
+		//
+		// private static final String bytesToHexString(byte[] bytes) {
+		// StringBuilder sb = new StringBuilder();
+		// for (int i = 0; i < bytes.length; i++) {
+		// String hex = Integer.toHexString(0xFF & bytes[i]);
+		// if (hex.length() == 1) {
+		// sb.append('0');
+		// }
+		// sb.append(hex);
+		// }
+		// return sb.toString();
+		// }
 	}
 
 	public static final void closeDiskCache() {
