89d6a7430f04acf026f95111275c8dab7e5ded42
armstrongli <jqxxboy@gmail.com>
2015-05-04 21:45:40 +0800

    change the algorithm of calculation in sample size

--- src/com/xxboy/activities/imageview/asynctasks/FullImageViewAsync.java
+++ src/com/xxboy/activities/imageview/asynctasks/FullImageViewAsync.java
@@ -16,7 +16,7 @@ public class FullImageViewAsync extends XBitmapAsyncTask {
 	@Override
 	protected Bitmap doInBackground() {
 		try {
-			return BitmapFactory.decodeFile(super.getImagePath());
+			return getFullImageOptions(getImagePath());
 		} catch (Exception e) {
 			Logger.log(e);
 			return null;
@@ -28,4 +28,26 @@ public class FullImageViewAsync extends XBitmapAsyncTask {
 		super.postExecute(result);
 	}
 
+	private static final Bitmap getFullImageOptions(String path) {
+		long maxMemSize = Runtime.getRuntime().maxMemory();
+		long realMemSize = -1;
+
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(path, options);
+		int with = options.outWidth, height = options.outHeight;
+		realMemSize = with * height / 2;
+
+		int sampleSize = 1;
+		long sampleSizeSqrt = realMemSize / maxMemSize;
+		if (sampleSizeSqrt > 0) {
+			do {
+				sampleSize <<= 1;
+			} while (sampleSize * sampleSize < sampleSizeSqrt);
+		}
+
+		options.inJustDecodeBounds = false;
+		options.inSampleSize = sampleSize;
+		return BitmapFactory.decodeFile(path, options);
+	}
 }
